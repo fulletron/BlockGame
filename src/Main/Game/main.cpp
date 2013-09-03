df28fae33cdf673e7d13ccdb6c97ea8fbf48dfc1
fulletron <kyle_pickering@yahoo.com>
2013-09-02 22:17:15 -0700

    main now shows that gsvector works to some degree.
    PS: LOOK INTO GETTING GTEST IN HERE! TDD!

--- src/Main/Game/main.cpp
+++ src/Main/Game/main.cpp
@@ -1,71 +1,26 @@
 #include <iostream>
 #include <conio.h>
 
-using namespace std;
-
 #include <boost\thread.hpp>
-#include <Utilities\utilities.h>
-
-class Fish
-{
-public:
-	Fish()
-	{
-		m_position = -1;
-		m_name = "NOT INITIALIZED";
-		m_icon = 7;
-	}
-
-	~Fish(){}
-	
-	int init(int a_pos, char * a_name, char a_icon)
-	{
-		m_position = a_pos;
-		m_name = a_name;
-		m_icon = a_icon;
-		return 0;
-	}
-
-	void swim()
-	{
-		m_position++;
-		if(m_position > 10)
-			m_position = 0;
-	}
-
-	void draw()
-	{
-		for(int i = 0; i < m_position; ++i)
-			printf(" ");
-		printf("%c", m_icon);
-		printf("\n");
-	}
-
-	char * getName(){ return m_name; }
-	int getPosition(){ return m_position; }
-
-private:
-	int				m_position;
-	char *			m_name;
-	char			m_icon;
-};
+#include "Utilities\utilities.h"
 
 int main()
 {
-	Fish f;
-	f.init( 2, "CHARLY", 22 );
-	for(int i = 0; i < 100; ++i)
-	{
-		f.swim();
-		f.draw();
-	}
-	/*
-	int testf = utest_function();
-	Foo f;
-	boost::thread test(boost::bind(&Foo::some_function, &f));
-	test.start_thread();
-	// added comment
-	printf("Hello World!!!");	
-	*/
+	GS::Utilities::ChunkManager g_man;
+	g_man.init();
+	GS::Utilities::Frame * pFrame = g_man.createFrame((_INT64)"trash000");
+	GS::Utilities::GSVector<void *> test;
+	test.init(pFrame, 12);
+	char * a = new char[1];
+	a[0] = 'a';
+	char * b = new char[1];
+	b[0] = 'b';
+	test.add(a);
+	test.add(b);
+	test.remove((_UINT32)1);
+
+	char t = *(char *)test.get(0);
+
+	printf( "hello world! %c \n", t );
 	return 0;
 }
