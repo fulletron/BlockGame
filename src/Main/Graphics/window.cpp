8442463830d61def38d26d9bed45b5461c65133f
fulletron <kyle_pickering@yahoo.com>
2013-11-01 18:36:47 -0700

    glfw doesn't need a seperate glew... removed the glew 1.10.0 project for now (W+L-M?)

--- src/Main/Graphics/window.cpp
+++ src/Main/Graphics/window.cpp
@@ -9,17 +9,17 @@ Window::Window()
 
 _UINT32 Window::init()
 {
-	glewInit();
+	//glewInit();
 	if(!glfwInit())
 		exit( EXIT_FAILURE );
-	/*
+
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 
 	/// SOIL USES DEPRECATED CALLS(IN CERTAIN FUNCTIONS). IT IS POSSIBLE YOU WILL NOT BE ABLE TO FORWARD COMPAT!
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_SAMPLES, 8);
-	*/
+
 
 	return 0;
 }
