78c0a97a00bb27c5bb2ec539456cda92332be04b
Magellan <kyle_pickering@yahoo.com>
2016-03-13 00:33:52 -0800

    fixed, added two necessary lines for texture usage

--- src/Main/Game/main.cpp
+++ src/Main/Game/main.cpp
@@ -68,6 +68,8 @@ int main(int argc, char * argv[])
 	glBindTexture(GL_TEXTURE_2D, tex);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	// GL_REPEAT, GL_MIRRORED_REPEAT, GL_CLAMP_TO_EDGE, GL_CLAMP_TO_BORDER
 	error = glGetError();
 
@@ -78,9 +80,9 @@ int main(int argc, char * argv[])
 
 	int width, height;
 	unsigned char* image =
-		SOIL_load_image("sample.png", &width, &height, 0, SOIL_LOAD_RGB);
+		SOIL_load_image("sample.png", &width, &height, 0, SOIL_LOAD_RGBA);
 	error = glGetError();
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
 	error = glGetError();
 	SOIL_free_image_data(image);
 
