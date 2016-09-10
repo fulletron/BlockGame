8c90ade7437e476a21a9f842ebc61aa8ebd11123
Magellan <kyle_pickering@yahoo.com>
2016-09-09 23:34:43 -0700

    adding flyload state. turn menu state into an entirely new structure

---
+++ src/Main/Game/characterchosenstates/flyloadstate.cpp
@@ -0,0 +1,22 @@
+#include "flyloadstate.h"
+
+namespace GS {
+namespace State {
+
+_INT32 FlyLoadState::onEnter(CharacterChosenState * a_g)
+{
+	return 0;
+}
+
+_INT32 FlyLoadState::onUpdate(CharacterChosenState * a_g)
+{
+	return 0;
+}
+
+_INT32 FlyLoadState::onExit(CharacterChosenState * a_g)
+{
+	return 0;
+}
+
+};
+};
---
+++ src/Main/Game/characterchosenstates/flyloadstate.h
@@ -0,0 +1,27 @@
+#ifndef __FLYLOADSTATE_H_
+#define __FLYLOADSTATE_H_
+
+#include <Utilities/typedefinitions.h>
+#include "../characterchosenstate.h"
+
+namespace GS {
+namespace State {
+
+class FlyLoadState : public IState<CharacterChosenState>
+{
+#pragma region STATE FUNCTIONALITY
+	typedef CharacterChosenState CURRENT_ISTATE_TEMPLATE;
+	// This is itself a state for the CURRENT_ISTATE_TEMPLATE State Machine.
+selective:
+public:
+	virtual _INT32 onEnter(CURRENT_ISTATE_TEMPLATE *);
+	virtual _INT32 onUpdate(CURRENT_ISTATE_TEMPLATE *);
+	virtual _INT32 onExit(CURRENT_ISTATE_TEMPLATE *);
+#pragma endregion
+
+};
+
+};
+};
+
+#endif
